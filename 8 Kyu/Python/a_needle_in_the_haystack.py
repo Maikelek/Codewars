da11f5e10f8a43aaacaed39bb1febb08b2de614a
Maikelek <maikelekmain@gmail.com>
2023-09-16 22:05:24 +0200

    ⚙ Added lvl 8 Katas

---
+++ 8 Kyu/Python/a_needle_in_the_haystack.py
@@ -0,0 +1,11 @@
+# Can you find the needle in the haystack?
+
+def find_needle(haystack):
+    index = 0
+
+    for item in haystack:
+        if item == "needle":
+            return f"found the needle at position {index}"
+        index += 1
+        if "needle" not in haystack:
+            return f"There is no needle in haystack"
---
+++ 8 Kyu/Python/convert_a_boolean_to_a_string.py
@@ -0,0 +1,7 @@
+# Implement a function which convert the given boolean value into its string representation.
+
+def boolean_to_string(b):
+    if b == False:
+        return "False"
+    else:
+        return "True"
