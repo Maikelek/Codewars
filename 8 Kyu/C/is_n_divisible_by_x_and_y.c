a890b7f7886936d5ffe61cb71413df1ae4d4d9b4
Maikelek <maikelekmain@gmail.com>
2023-09-25 21:51:39 +0200

    ⚙ Added lvl 8 Kata in C

---
+++ 8 Kyu/C/is_n_divisible_by_x_and_y.c
@@ -0,0 +1,9 @@
+// Create a function that checks if a number n is divisible by two numbers x AND y. All inputs are positive, non-zero numbers.
+
+#include <stdbool.h>
+
+bool isDivisible(int n, int x, int y) {
+
+    return (n % x == 0 && n % y == 0 ? true:false);
+
+}
