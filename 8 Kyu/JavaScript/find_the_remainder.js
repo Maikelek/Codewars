a29a02bc1dde940f05824b724146305fa79d70f7
Marián Zelinka <maikelekmain@gmail.com>
2023-10-07 15:43:43 +0200

    Level 8 katas in JS

---
+++ 8 Kyu/JavaScript/find_the_remainder.js
@@ -0,0 +1,15 @@
+// Write a function that accepts two integers and returns the remainder of dividing the larger value by the smaller value.
+// Division by zero should return NaN.
+
+function remainder(n, m){
+	if (n > m) {
+		return n % m
+	}
+	else if (m > n || m === n) {
+		return m % n
+	}
+	else {
+		return NaN
+	}
+	
+  }
