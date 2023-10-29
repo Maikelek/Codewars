127fe3d4c37fb2debf0390b2d33c06244aa4e20b
Marián Zelinka <maikelekmain@gmail.com>
2023-10-29 13:28:48 +0100

    ⚙ Added 2 lvl 7 katas in JS

---
+++ 7 Kyu/Javascript/find_the_capitals.js
@@ -0,0 +1,13 @@
+// Write a function that takes a single string (word) as argument. 
+// The function must return an ordered list containing the indexes of all capital letters in the string.
+
+function capitals(word) {
+    let output = [];
+    
+    for (let i = 0; i < word.length; i++) {
+        if (word[i] === word[i].toUpperCase()) {
+            output.push(i)
+        }
+    }
+    return output
+}
